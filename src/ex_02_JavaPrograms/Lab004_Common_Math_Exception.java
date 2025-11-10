b53bcf4836c99148697be1a85fbc0d79dedeb575
PrashQA <billimoggaprashanth@gmail.com>
2025-11-10 15:16:09 +0530

    Test class 2

---
+++ src/ex_02_JavaPrograms/Lab004_Common_Math_Exception.java
@@ -0,0 +1,8 @@
+package ex_02_JavaPrograms;
+
+public class Lab004_Common_Math_Exception {
+    public static void main(String[] args) {
+        System.out.println(10/0);
+        // Exception in thread "main" java.lang.ArithmeticException
+    }
+}
---
+++ src/ex_02_JavaPrograms/Lab008_InterviewQnA.java
@@ -0,0 +1,11 @@
+package ex_02_JavaPrograms;
+
+public class Lab008_InterviewQnA {
+
+
+        public static void main(String[] args) {
+            int _2 = 123;
+            System.out.println(_2);
+
+        }
+    }
