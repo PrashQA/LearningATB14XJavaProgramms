b00901a01ccbd1bb7e174014003a0261e41d3a32
PrashQA <billimoggaprashanth@gmail.com>
2025-11-10 15:23:02 +0530

    Test class 3

---
+++ src/ex03_JavaPrograms/Lab011_Literals.java
@@ -0,0 +1,22 @@
+package ex03_JavaPrograms;
+
+public class Lab011_Literals {
+
+
+public static void main(String[] args) {
+
+    int age = 65;
+    // Literal - Integral
+
+    final float pi = 3.14f;
+    //  Literal - float
+
+
+    final int girl_age = 18;
+    //girl_age = 19;
+
+
+}
+
+
+}
---
+++ src/ex03_JavaPrograms/Lab013_Literals.java
@@ -0,0 +1,8 @@
+package ex03_JavaPrograms;
+
+public class Lab013_Literals {
+    public static void main(String[] args) {
+        int Enum = 9;
+        System.out.println(Enum);
+    }
+}
