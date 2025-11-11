b3ba6e4f190c5a96f7b23bc9a499740a777401be
PrashQA <billimoggaprashanth@gmail.com>
2025-11-11 15:08:41 +0530

    Test class 12

---
+++ src/ex12_Do_while/Lab078_Do_while.java
@@ -0,0 +1,12 @@
+package ex12_Do_while;
+
+public class Lab078_Do_while {
+    public static void main(String[] args) {
+        int a = 1;
+        do{
+            System.out.println("Body!");
+            System.out.println(a);
+            a++;
+        }while (a<10);
+    }
+}
---
+++ src/ex12_Do_while/Lab080_Do_while.java
@@ -0,0 +1,16 @@
+package ex12_Do_while;
+
+public class Lab080_Do_while {
+    public static void main(String[] args) {
+        // Web Automation
+        int number = 0;
+        // 0 -> it can be 0 to 10 - elements or students
+        do{
+            System.out.println(number);
+            // callFunction(number);
+            // close the excel file, open the file.
+            number++;
+        }while (number < 0);
+
+    }
+}
