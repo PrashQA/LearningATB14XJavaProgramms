6eda46c06217b3f66d15a7aaf2abda6c29d8e64a
PrashQA <billimoggaprashanth@gmail.com>
2025-11-11 16:06:30 +0530

    Test class 18

---
+++ src/ex18_OOPS_Constructors/LabInstancevsLocal.java
@@ -0,0 +1,9 @@
+package ex18_OOPS_Constructors;
+
+public class LabInstancevsLocal {
+    String name; // instance variable or attribute or data members or properties
+
+    void hello(){
+        String name = "Pramod";
+    }
+}
