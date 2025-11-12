d6efa5ed63d7d9076b8a5100e0b98e7ce0b61653
PrashQA <billimoggaprashanth@gmail.com>
2025-11-12 11:08:28 +0530

    Test class 26

---
+++ src/ex26_Abstraction/Lab142_InterfaceP2.java
@@ -0,0 +1,32 @@
+package ex26_Abstraction;
+
+public class Lab142_InterfaceP2 {
+    interface I1{
+        void icm1();
+        void icm2();
+    }
+
+
+    interface I2{
+        void icm3();
+    }
+
+    class P implements I1,I2 {
+
+
+        @Override
+        public void icm1() {
+
+        }
+
+        @Override
+        public void icm2() {
+
+        }
+
+        @Override
+        public void icm3() {
+
+        }
+    }
+}
