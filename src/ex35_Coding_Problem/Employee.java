0736c4fa8e04a82fed06e96a7a8d9287e42ccf29
PrashQA <billimoggaprashanth@gmail.com>
2025-11-16 09:31:24 +0530

    Problem1

---
+++ src/ex35_Coding_Problem/Employee.java
@@ -0,0 +1,58 @@
+package ex35_Coding_Problem;
+
+public class Employee {
+    // Private attributes
+    private int id;
+    private String name;
+    private double salary;
+
+    // Constructor
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    // Setter methods
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // Method to display employee details
+    public void displayDetails() {
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    // Main method to test the class
+    public static void main(String[] args) {
+        // Creating an Employee object
+        Employee emp = new Employee(1, "John Doe", 50000);
+
+        // Displaying employee details
+        emp.displayDetails();
+    }
+
+}
